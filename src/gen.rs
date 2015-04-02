e7d6a5f766aee5e4fe253c70ee1cdef955ac608d
Michael Wu <mwu@mozilla.com>
2015-04-02 02:54:44 -0400

    Add support for bitfield setter generation

--- src/gen.rs
+++ src/gen.rs
@@ -617,6 +617,9 @@ fn cstruct_to_rs(ctx: &mut GenCtx, name: String, layout: Layout,
     let mut unnamed: u32 = 0;
     let mut bitfields: u32 = 0;
 
+    let id = rust_type_id(ctx, name.clone());
+    let id_ty = P(mk_ty(ctx, false, vec!(id.clone())));
+    let mut setters = vec!();
     for m in members.iter() {
         let (opt_rc_c, opt_f) = match m {
             &CompMember::Field(ref f) => { (None, Some(f)) }
@@ -632,6 +635,14 @@ fn cstruct_to_rs(ctx: &mut GenCtx, name: String, layout: Layout,
                 }
                 None => rust_type_id(ctx, f.name.clone())
             };
+
+            let mut offset: u32 = 0;
+            if let Some(ref bitfields) = f.bitfields {
+                for &(ref bf_name, bf_size) in bitfields.iter() {
+                    setters.push(gen_bitfield_method(ctx, &f_name, bf_name, &f.ty, offset as usize, bf_size as usize));
+                    offset += bf_size;
+                }
+            }
 
             let f_ty = P(cty_to_rs(ctx, &f.ty));
 
@@ -658,6 +669,23 @@ fn cstruct_to_rs(ctx: &mut GenCtx, name: String, layout: Layout,
                                         c.layout, c.members.clone(), c.args.clone()).into_iter());
             }
         }
+    }
+    if !setters.is_empty() {
+        extra.push(P(ast::Item {
+            ident: ctx.ext_cx.ident_of(""),
+            attrs: vec!(),
+            id: ast::DUMMY_NODE_ID,
+            node: ast::ItemImpl(
+                ast::Unsafety::Normal,
+                ast::ImplPolarity::Positive,
+                empty_generics(),
+                None,
+                id_ty.clone(),
+                setters
+            ),
+            vis: ast::Inherited,
+            span: ctx.span
+        }));
     }
 
     let ctor_id = if fields.is_empty() { Some(ast::DUMMY_NODE_ID) } else { None };
@@ -692,7 +720,6 @@ fn cstruct_to_rs(ctx: &mut GenCtx, name: String, layout: Layout,
         }
     );
 
-    let id = rust_type_id(ctx, name.clone());
     let struct_def = P(ast::Item { ident: ctx.ext_cx.ident_of(&id[..]),
         attrs: vec!(mk_repr_attr(ctx, layout), mk_deriving_copy_attr(ctx)),
         id: ast::DUMMY_NODE_ID,
@@ -708,7 +735,7 @@ fn cstruct_to_rs(ctx: &mut GenCtx, name: String, layout: Layout,
             ast::ImplPolarity::Positive,
             empty_generics(),
             None,
-            P(mk_ty(ctx, false, vec!(id))),
+            id_ty,
             methods
         );
         items.push(
@@ -934,6 +961,30 @@ fn gen_comp_methods(ctx: &mut GenCtx, data_field: &str, data_offset: usize,
         }
     }
     methods
+}
+
+fn gen_bitfield_method(ctx: &mut GenCtx, bindgen_name: &String,
+                       field_name: &String, field_type: &Type,
+                       offset: usize, width: usize) -> ast::ImplItem {
+    let input_type = if width > 16 {
+        ctx.ext_cx.ident_of("u32")
+    } else if width > 8 {
+        ctx.ext_cx.ident_of("u16")
+    } else if width > 1 {
+        ctx.ext_cx.ident_of("u8")
+    } else {
+        ctx.ext_cx.ident_of("bool")
+    };
+
+    let field_type = cty_to_rs(ctx, &field_type);
+    let setter_name = ctx.ext_cx.ident_of(format!("set_{}", field_name).as_slice());
+    let bindgen_ident = ctx.ext_cx.ident_of(bindgen_name.as_slice());
+    ast::MethodImplItem(quote_method!(&ctx.ext_cx,
+        pub fn $setter_name(&mut self, val: $input_type) {
+            self.$bindgen_ident &= !(((1 << $width) - 1) << $offset);
+            self.$bindgen_ident |= (val as $field_type) << $offset;
+        }
+    ))
 }
 
 // Implements std::default::Default using std::mem::zeroed.
