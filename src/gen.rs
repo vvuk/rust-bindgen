628e703d67fc57bc6b958aee7a07f975e01b572b
Michael Wu <mwu@mozilla.com>
2015-05-24 19:53:17 -0400

    Generate bitfields correctly for typedefs

--- src/gen.rs
+++ src/gen.rs
@@ -998,7 +998,7 @@ fn cunion_to_rs(ctx: &mut GenCtx, name: String, derive_debug: bool, layout: Layo
     }
 
     let ci = Rc::new(RefCell::new(CompInfo::new(name.clone(), name.clone(), CompKind::Union, members.clone(), vec!(), vec!(), layout)));
-    let union = TNamed(Rc::new(RefCell::new(TypeInfo::new(name.clone(), TComp(ci)))));
+    let union = TNamed(Rc::new(RefCell::new(TypeInfo::new(name.clone(), TComp(ci), layout))));
 
     // Nested composites may need to emit declarations and implementations as
     // they are encountered.  The declarations end up in 'extra' and are emitted
