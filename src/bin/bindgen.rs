93369fd59f5c38097132589c3b75c6e12fdf75a8
Ben Foppa <benjamin.foppa@gmail.com>
2015-05-25 11:00:28 -0400

    Remove +'static when writing bindings

--- src/bin/bindgen.rs
+++ src/bin/bindgen.rs
@@ -29,7 +29,7 @@ impl Logger for StdLogger {
 
 enum ParseResult {
     CmdUsage,
-    ParseOk(BindgenOptions, Box<io::Write+'static>),
+    ParseOk(BindgenOptions, Box<io::Write>),
     ParseErr(String)
 }
 
