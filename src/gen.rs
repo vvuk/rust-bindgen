e7c3a368f303d1f676312ec3bef99b4a07f2be2f
Michael Wu <mwu@mozilla.com>
2015-07-23 17:44:28 -0400

    Convert signed chars to libc::c_char

--- src/gen.rs
+++ src/gen.rs
@@ -1640,7 +1640,7 @@ fn cty_to_rs(ctx: &mut GenCtx, ty: &Type, allow_bool: bool) -> ast::Ty {
                 };
                 mk_ty(ctx, false, vec!(ty_name.to_owned()))
             },
-            ISChar |
+            ISChar => mk_ty(ctx, true, vec!("libc".to_string(), "c_char".to_string())),
             IInt |
             IShort |
             ILongLong => mk_ty(ctx, false, vec!(format!("i{}", layout.size * 8))),
